c83586c4aa5c3771f242e86bfdb216126923daa3
siddhu-bhima <siddhubhima18@gmail.com>
2026-07-13 22:35:15 +0530

    Updated commands

--- commands.hpp
+++ commands.hpp
@@ -5,5 +5,4 @@ void commit(const std::string &message);
 void log(void);
 void initialize_repo(void);
 void checkout(const std::string &ref, bool force = false);
-//void merge(const std::string &target_branch);
 void status(void);
